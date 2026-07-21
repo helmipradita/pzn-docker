7ae5c0b13492a5321396840988836da9b5049923
Helmi Pradita <helmipraditaa@gmail.com>
2026-07-21 10:34:26 +0700

    chore: init .gitignore with env and mongo-data, add bind mount script
    Ignore local Mongo data directory and environment files.

---
+++ docker-bind-mounts.sh
@@ -0,0 +1,1 @@
+docker container create --name mongodata --publish 27018:27017 --mount "type=bind,source=/home/z0nk/Developments/PZN/DOCKER/belajar-docker-dasar/mongo-data,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=helmipradita --env MONGO_INITDB_ROOT_PASSWORD=helmipradita mongo:latest
