be9643bbbcdc1ec5c02c1bbded68b3bfccaca8d9
Helmi Pradita <helmipraditaa@gmail.com>
2026-07-23 10:02:44 +0700

    feat(dockerfile): add RUN instruction example
    Dockerfile demonstrating RUN commands: mkdir, echo, and cat
    showing layer execution in image build.

--- belajar-docker-dockerfile/script.sh
+++ belajar-docker-dockerfile/script.sh
@@ -1,3 +1,9 @@
+# FROM Instruction
 docker build -t helmipradita/from from
 
 docker image ls
+
+# RUN Instruction
+docker build -t helmipradita/run run
+
+docker build -t helmipradita/run run --progress=plain --no-cache
