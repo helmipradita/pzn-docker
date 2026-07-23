2f16a9963d9419fa8c8b41cccf66444fdabb8cb6
Helmi Pradita <helmipraditaa@gmail.com>
2026-07-23 10:06:36 +0700

    feat(dockerfile): add CMD instruction example
    Dockerfile with CMD to print hello world text after build.

--- belajar-docker-dockerfile/script.sh
+++ belajar-docker-dockerfile/script.sh
@@ -7,3 +7,14 @@ docker image ls
 docker build -t helmipradita/run run
 
 docker build -t helmipradita/run run --progress=plain --no-cache
+
+# CMD Instruction
+docker build -t helmipradita/command command
+
+docker image inspect helmipradita/command
+
+docker container create --name command helmipradita/command
+
+docker container start command
+
+docker container logs command
